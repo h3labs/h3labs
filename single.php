aeb881c3b16ce486daba395cbc1431b78df2f60b
Guillermo <gavargas22@gmail.com>
2013-09-24 18:04:28 -0600

    Major changes

--- single.php
+++ single.php
@@ -4,19 +4,25 @@
   		<?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
   		<div id="descr-title"><h1><?php the_title(); ?></h1></div>
+  		<div class="author-meta-single inline"><i class="icon-user"></i>  By <?php the_author_posts_link(); ?></div><div class="post-meta-date inline"><i class="icon-calendar"></i>  On <?php the_date(); ?></div>
   	</div>
   	<div id="page-main-body-wrapper" class="clearfix">
   		<div class="row-fluid">   
   			<div class="span8"><?php the_content(); ?></div>
   			<?php edit_post_link('Edit'); ?>
-  			<?php endwhile; ?>
-    		<?php endif; ?>
-    		<div class="span2 offset1">
-    			<div class="author-meta"><i class="icon-user"></i> <?php the_author(); ?></div>
-    			<a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;">Share on Facebook</a>
-    		</div>
+    		<!--<div class="span2 offset1">
+    			<div class="span12">
+    				<h3>About the author</h3>
+    				<div class="author-meta-image"><img src="http://distilleryimage8.ak.instagram.com/9b6d232a256811e387c422000ae911c7_7.jpg" class="img-polaroid" /></div>
+    				<div class="author-meta-name">Jesus Mendoza</div>
+    				<div class="author-meta-bio"><?php the_author_meta('description'); ?></div>
+    			</div>
+    			<!--<div class="span3"><a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;">Share on Facebook</a></div>-->
+    		<!--</div>-->
 		</div>
 	</div>
+	<?php endwhile; ?>
+    <?php endif; ?>
 </div>
 <div class="container"><?php comments_template(); ?></div>
 
