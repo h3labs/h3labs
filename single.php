60109f38297dd60576200b3f08ae6e9c8bd3a5b9
Guillermo <gavargas22@gmail.com>
2013-10-29 16:00:45 -0600

    Social 2

--- single.php
+++ single.php
@@ -8,7 +8,7 @@
   		<!--<div><span style="color:#333; font-size:11px;"><i class="icon-share"></i> Share</span></div>-->
   		<ul class="inline" style="padding-top:8px;">
   			<li class="social-btns" style="padding-left:0;"><a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/facebook_button.png" /></a></li>
-  			<li class="social-btns" style="padding-left:0;"><a href="https://twitter.com/share?url=<?php the_permalink(); ?>" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/twitter_button.png" /></a></li>
+  			<li class="social-btns" style="padding-left:0;"><a href="https://twitter.com/share?url=<?php the_permalink(); ?>" data-text="Just check this tweet button" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/twitter_button.png" /></a></li>
   		</ul>
   	</div>
   	<div id="page-main-body-wrapper" class="clearfix container">
