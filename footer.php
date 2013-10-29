e29a5b242622b3953f115dab56beefabd7806eda
Guillermo <gavargas22@gmail.com>
2013-10-29 15:55:22 -0600

    Social

--- footer.php
+++ footer.php
@@ -30,5 +30,6 @@
     <script src="<?php bloginfo('stylesheet_directory'); ?>/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/init.js"></script>
     <script src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/jquery.infinitescroll.min.js"></script>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   </body>
 </html>
--- single.php
+++ single.php
@@ -5,19 +5,23 @@
         <?php while (have_posts()) : the_post(); ?>
   		<div id="descr-title"><h1><?php the_title(); ?></h1></div>
   		<div class="author-meta-single inline"><i class="icon-user"></i>  By <?php the_author_posts_link(); ?></div><div class="post-meta-date inline"><i class="icon-calendar"></i>  On <?php the_date(); ?></div>
+  		<!--<div><span style="color:#333; font-size:11px;"><i class="icon-share"></i> Share</span></div>-->
+  		<ul class="inline" style="padding-top:8px;">
+  			<li class="social-btns" style="padding-left:0;"><a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/facebook_button.png" /></a></li>
+  			<li class="social-btns" style="padding-left:0;"><a href="https://twitter.com/share?url=<?php the_permalink(); ?>" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/twitter_button.png" /></a></li>
+  		</ul>
   	</div>
-  	<div id="page-main-body-wrapper" class="clearfix">
+  	<div id="page-main-body-wrapper" class="clearfix container">
   		<div class="row-fluid">   
   			<div class="span8"><?php the_content(); ?></div>
   			<?php edit_post_link('Edit'); ?>
-    		<div class="span2 offset1 post-toolkit">
-    			
-    			<!--<div class="span3"><a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;">Share on Facebook</a></div>-->
-		</div>
+    		<div class="span2 post-toolkit">
+    			<!--div class="container"><i class="icon-comment"></i> n Comments</div>-->
+    		</div>
 	</div>
 	<?php endwhile; ?>
     <?php endif; ?>
-</div>
+	</div>
 <div class="container"><?php comments_template(); ?></div>
 
     
